856098a748d818151f63df9893af576d58b527d8
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-12-09 01:31:31 +0300

    edit navbar

--- src/components/NavbarComponent.tsx
+++ src/components/NavbarComponent.tsx
@@ -14,10 +14,10 @@ import "../styles/navbar.css";
 const NavbarComponent = () => {
   return (
     <div id="navbarComponent">
-      <Navbar id="navbarStyling" bg="dark" expand={false}>
+      <Navbar id="navbarStyling" bg="dark" expand={false} fixed="top">
         <Container fluid>
           <Navbar.Brand id="navbarTitle" href="#">
-            Navbar Offcanvas
+            Grumpy Gorilla Gang
           </Navbar.Brand>
           <Navbar.Toggle id="menuBtn" aria-controls="offcanvasNavbar" />
           <Navbar.Offcanvas
@@ -27,33 +27,15 @@ const NavbarComponent = () => {
           >
             <Offcanvas.Header closeButton>
               <Offcanvas.Title id="offcanvasNavbarLabel">
-                Offcanvas
+                Grumpy Gorilla Gang
               </Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body>
               <Nav className="justify-content-end flex-grow-1 pe-3">
-                <Nav.Link href="#action1">Home</Nav.Link>
-                <Nav.Link href="#action2">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
-                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action4">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action5">
-                    Something else here
-                  </NavDropdown.Item>
-                </NavDropdown>
+                <Nav.Link href="#landingComponent">Home</Nav.Link>
+                <Nav.Link href="#roadmapContainer">Roadmap</Nav.Link>
+                <Nav.Link href="#aboutComponent">About us</Nav.Link>
               </Nav>
-              <Form className="d-flex">
-                <FormControl
-                  type="search"
-                  placeholder="Search"
-                  className="me-2"
-                  aria-label="Search"
-                />
-                <Button variant="outline-success">Search</Button>
-              </Form>
             </Offcanvas.Body>
           </Navbar.Offcanvas>
         </Container>
